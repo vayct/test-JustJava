c09dad858e43d0ec809eb9ed5f6d2388a46a4c23
vayct <vayct@slu.edu>
2017-05-24 15:02:22 -0600

    Add Intent with email sending order

--- app/src/main/java/com/example/jetsu/justjava/MainActivity.java
+++ app/src/main/java/com/example/jetsu/justjava/MainActivity.java
@@ -1,54 +1,118 @@
 package com.example.jetsu.justjava;
 
-import android.icu.text.NumberFormat;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import static android.os.Build.VERSION_CODES.N;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int quantity = 0;
+    private int quantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void submitOrder(View view) {
 
-        String priceMessage = "Amount due $ " + quantity*5 +"\nThank you!";
-        displayMessage(priceMessage);
+
+        int price = 5;
+
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_check_box);
+        boolean creamCheck = whippedCreamCheckBox.isChecked();
+
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_check_box);
+        boolean chocolateCheck = chocolateCheckBox.isChecked();
+
+        if(creamCheck)
+            price++;
+
+        if(chocolateCheck)
+            price = price +2;
+
+
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        String name =  nameField.getText().toString();
+
+
+        String priceMessage = createOrderSummary(price,creamCheck,chocolateCheck, name);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java Order for " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if(intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+
+
+ /**
+        //for checking out example intent
+
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("geo:147.6, -123.3"));
+
+        if(i.getIntent().resolveActivity(getPackageManager()) != null)
+            startActivity(i);
+ */
     }
+
+
+
+
+
+
+
+
 
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText(Integer.toString(number));
-
     }
 
-    public void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText("$ " + number);
-    }
 
     public void increment(View view) {
+
+
+        if(quantity == 100) {
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         quantity++;
         display(quantity);
     }
 
     public void decrement(View view) {
+
+
+        if(quantity == 1) {
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         quantity--;
         display(quantity);
     }
 
-    public void displayMessage(String message)
-    {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
 
+
+    private String createOrderSummary(int price, boolean cream, boolean chocolate, String name){
+        String priceMessage = "Name: " + name;
+        priceMessage = priceMessage + "\nAdd whipped cream? " + cream;
+        priceMessage = priceMessage + "\nAdd chocolate? " + chocolate;
+        priceMessage = priceMessage + "\nQuantity: " + quantity;
+        priceMessage = priceMessage + "\nTotal: $" + quantity*price;
+        return priceMessage;
     }
+
+
 }
